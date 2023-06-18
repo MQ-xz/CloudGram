ba62b07ab32793ae334094d99f709f5381c5620d
Mahshooq Zubair <kmahshooq@gmail.com>
2023-06-18 10:14:52 +0530

    Update: login screen

--- src/screens/login_screen.py
+++ src/screens/login_screen.py
@@ -1,13 +1,32 @@
 """LoginScreen"""
 
 from kivymd.uix.screen import MDScreen
+from kivymd.uix.boxlayout import MDBoxLayout
 
 
 class LoginScreen(MDScreen):
     """LoginScreen"""
 
-    def login(self):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.phone_number = None
+        self.otp = None
+
+    def send_otp(self, phone_number):
         """login"""
-        print(self.ids.phone_number.text)
-        print("Let's login")
+        print("Let's login : LoginScreen")
+        self.phone_number = phone_number.ids.phone_number.text
+        print(self.phone_number)
+        self.ids.login_screen_layout.remove_widget(phone_number)
+        self.ids.login_screen_layout.add_widget(EnterOTP())
+
+    def login(self, otp):
+        """login"""
+        print("Let's login : LoginScreen")
+        self.otp = otp.ids.otp.text
+        print(self.otp)
         self.manager.current = "home"
+
+
+class EnterOTP(MDBoxLayout):
+    """EnterOTP"""
