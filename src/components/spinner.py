eaf65bef15c702bae5b24b9e2d0afd240247f8fa
Mahshooq Zubair <kmahshooq@gmail.com>
2023-06-18 11:37:02 +0530

    Update: all

---
+++ src/components/spinner.py
@@ -0,0 +1,5 @@
+from kivymd.uix.spinner import MDSpinner
+
+
+class Spinner(MDSpinner):
+    """Spinner"""
--- src/screens/login_screen.py
+++ src/screens/login_screen.py
@@ -1,7 +1,10 @@
 """LoginScreen"""
 
+from time import sleep
+from functools import partial
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.boxlayout import MDBoxLayout
+from kivy.clock import Clock
 
 
 class LoginScreen(MDScreen):
@@ -9,16 +12,31 @@ class LoginScreen(MDScreen):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.phone_number = None
+        self.phone_number = ""
         self.otp = None
+
+    def otp_request(self, phone_number, *args):
+        """send otp request to telegram"""
+        print("hehe")
+        sleep(2)
+        print(self.phone_number, "otp_request")
+        # add error
+        phone_number.theme_text_color = "Error"
+        phone_number.ids.send_otp_button.disabled = False
+        phone_number.ids.send_otp_button.text = "Reending"
+        # self.ids.login_screen_layout.remove_widget(phone_number)
+        # self.ids.login_screen_layout.add_widget(EnterOTP())
 
     def send_otp(self, phone_number):
         """login"""
         print("Let's login : LoginScreen")
+
         self.phone_number = phone_number.ids.phone_number.text
-        print(self.phone_number)
-        self.ids.login_screen_layout.remove_widget(phone_number)
-        self.ids.login_screen_layout.add_widget(EnterOTP())
+        # set loading
+        phone_number.ids.send_otp_button.disabled = True
+        phone_number.ids.send_otp_button.text = "Sending"
+
+        Clock.schedule_once(partial(self.otp_request, phone_number))
 
     def login(self, otp):
         """login"""
