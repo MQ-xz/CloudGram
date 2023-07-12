a661921e3cdf83461ef275d54f93b340ca16caad
Mahshooq Zubair <kmahshooq@gmail.com>
2023-07-13 00:51:27 +0530

    Fix: lint

--- src/utils/getFileIcon.js
+++ src/utils/getFileIcon.js
@@ -1,5 +1,5 @@
 export default function getFileIcon(filename) {
-    const fileExtension = filename.split('.').reverse()[0]
+    const fileExtension = filename.split(".").reverse()[0];
 
     if (fileTypesMap.image.includes(fileExtension)) {
         return iconsMap.image;
@@ -24,18 +24,18 @@ export default function getFileIcon(filename) {
 }
 
 const iconsMap = {
-    image: 'https://img.icons8.com/fluency/48/image.png',
-    audio: 'https://img.icons8.com/color/48/audio-file.png',
-    video: 'https://img.icons8.com/color/48/cinema---v1.png',
-    document: 'https://img.icons8.com/color/48/document--v1.png',
-    archive: 'https://img.icons8.com/color/48/archive.png',
-    misc: 'https://img.icons8.com/color/48/file.png'
-}
+    image: "https://img.icons8.com/fluency/48/image.png",
+    audio: "https://img.icons8.com/color/48/audio-file.png",
+    video: "https://img.icons8.com/color/48/cinema---v1.png",
+    document: "https://img.icons8.com/color/48/document--v1.png",
+    archive: "https://img.icons8.com/color/48/archive.png",
+    misc: "https://img.icons8.com/color/48/file.png",
+};
 
 const fileTypesMap = {
-    image: ['png', 'jpg', 'jpeg', 'gif', 'webp'],
-    audio: ['mp3', 'aac', 'wav'],
-    video: ['mp4', 'webm', 'mkv'],
-    document: ['pdf', 'doc', 'xls', 'xlsx'],
-    archive: ['iso', 'zip', 'rar', 'tar', 'tar.gz']
-}
+    image: ["png", "jpg", "jpeg", "gif", "webp"],
+    audio: ["mp3", "aac", "wav"],
+    video: ["mp4", "webm", "mkv"],
+    document: ["pdf", "doc", "xls", "xlsx"],
+    archive: ["iso", "zip", "rar", "tar", "tar.gz"],
+};
